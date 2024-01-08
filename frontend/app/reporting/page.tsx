c3623300e3a6239267f0314457d1522797cee25a
essiva <essimaria.varjonen@gmail.com>
2024-01-08 13:46:20 +0200

    added top section styling to reporting & instructions page

--- frontend/app/reporting/page.tsx
+++ frontend/app/reporting/page.tsx
@@ -1,5 +1,15 @@
+import TopSection from "../components/topSection/topSection";
+
 export default function Reporting() {
-  return(
-    <div>Test</div>
-  ) 
+  return (
+    <div>
+      <TopSection
+        header="Reporting & Instructions"
+        description="Following information is updated for each winter navigation season,
+        unless otherwise mentioned. This information is covering the Baltic
+        Sea area. When icebreaking activity and traffic restrictions are off,
+        updated information is not available."
+      />
+    </div>
+  );
 }
--- frontend/app/weather/page.tsx
+++ frontend/app/weather/page.tsx
@@ -1,5 +1,14 @@
+import TopSection from "../components/topSection/topSection";
+
 export default function Weather() {
-  return(
-    <div>Weather</div>
-  ) 
+  return (
+    <div>
+      <TopSection
+        header="Ice & Weather"
+        description="Following information is updated every day during the winter navigation season, 
+        unless otherwise mentioned. This information is covering the Baltic Sea area. 
+        When icebreaking activity and traffic restrictions are off, updated information is not available."
+      />
+    </div>
+  );
 }
